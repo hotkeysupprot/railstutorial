80ff18cf7ed550d971f49268f1a0f85646c8d53c
Nagahora Toyoaki <hot87hara@gmail.com>
2016-11-27 10:54:54 +0900

    Finish 7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,12 +6,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name:  "",
-                               email: "user@invalid",
-                               password:              "foo",
-                               password_confirmation: "bar" }
+      post users_path, user: {name: "",
+                              email: "user@invalid",
+                              password: "foo",
+                              password_confirmation: "bar"}
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
   end
 
   # ユーザー作成成功
@@ -19,11 +21,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_difference 'User.count', 1 do
       # post_via_redirectメソッド: POSTリクエストを送信し、リダイレクトする。複数回リダイレクトがあるなら最後まで行く。
-      post_via_redirect users_path, user: { name:  "Example User",
-                                            email: "user@example.com",
-                                            password:              "password",
-                                            password_confirmation: "password" }
+      post_via_redirect users_path, user: {name: "Example User",
+                                           email: "user@example.com",
+                                           password: "password",
+                                           password_confirmation: "password"}
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
